d54f1023b5925b7539caf0bbb18a4be8a838911b
Lucas Gardini <lucasgardini.dias@hotmail.com>
2021-04-14 12:53:09 -0300

    Fixed some localtunnel issues

--- server/app.js
+++ server/app.js
@@ -70,18 +70,18 @@ app.get("**", (req, res) => {
 });
 
 app.listen(process.env.PORT || port, async () => {
-	// console.log(" ⌛ Waiting for localtunnel");
+	console.log(" ⌛ Waiting for localtunnel");
 
-	// const server = await host({ port: 8080, subdomain: "api-orderify" });
+	const server = await host({ port: 8080, subdomain: "orderify" });
 	// const websocket = await host({ port: 8081, subdomain: "ws-orderify" });
 	// const mobile = await host({ port: 6924, subdomain: "app-orderify" });
 
-	// const server_url = server.url;
+	const server_url = server.url;
 	// const websocket_url = websocket.url;
 	// const mobile_url = mobile.url;
 
 	// console.log("-----------------------------");
-	// console.log(` 🛅 Server hosting in ${server_url.yellow.bgBlack}`);
+	console.log(` 🛅 Server hosting in ${server_url.yellow.bgBlack}`);
 	// console.log(` 🔌 WebSocket hosting in ${websocket_url.yellow.bgBlack}`);
 	// console.log(` 📱 Nuxt server listening in ${mobile_url.yellow.bgBlack}`);
 	// console.log("-----------------------------");
